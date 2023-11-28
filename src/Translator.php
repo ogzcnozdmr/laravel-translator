8da893aa6ebf58cf353cf010ddf9a3760e26b3f8
Oğuzcan Özdemir <o.ozdmr.40@gmail.com>
2023-11-28 13:17:23 +0300

    new updates

--- src/Translator.php
+++ src/Translator.php
@@ -8,12 +8,19 @@ class Translator
 {
     private TranslateClient $trs;
 
-    private $targetDirectoryOperator = '/';//\\
-    private $sourcePrefix = '../lang/';
+    private string $targetDirectoryOperator = '/';//\\
+    private string $sourcePrefix = '../lang/';
 
-    private $source, $target;
+    private string $jsSourcePath = '';
+    private bool $jsLanguageCreator = false;
 
-    private $sourcePath, $realSourcePath;
+    private array $translate = [];
+
+    private string $source;
+    private array $target;
+
+    private string $sourcePath;
+    private string $realSourcePath;
 
     /**
      * @param string $googleKey
@@ -30,20 +37,21 @@ class Translator
             $target = [$target];
         }
         $this->target = $target;
+        $this->createPaths();
     }
 
     /**
      * Translateyi başlatır
      * @return void
      */
-    public function get() : void
+    public function __create() : void
     {
         /*
          * Pathları oluşturur
          */
         foreach ($this->target as $targetValue) {
             $realTargetPath = $this->realTargetPath($this->realSourcePath, $targetValue);
-            $result = array_reverse($this->getDirContents());
+            $result = array_reverse($this->getDirContents($this->sourcePath));
             //ana klasörü oluşturur
             if (!file_exists($realTargetPath)) {
                 mkdir($realTargetPath);
@@ -59,23 +67,59 @@ class Translator
                 } else {
                     //dosyayı okuyup translate yapacak
                     $getData = require $value;
-                    //dosyayı yoksa oluşturur
-                    $file = fopen($realTargetPath . $valueMainPath, 'w');
                     $writeData = "<?php " . PHP_EOL . PHP_EOL . "return [" . PHP_EOL;
                     foreach ($getData as $dataKey => $dataValue) {
-                        //Dil çevrimi yapar
+                        /*
+                         * __create language text
+                         */
                         $result = $this->trs->translate($dataValue, [
                             'source' => $this->source,
                             'target' => $targetValue
                         ]);
+                        if ($this->jsLanguageCreator) {
+                            $this->translate[$targetValue][
+                                str_replace(
+                                    $this->targetDirectoryOperator,
+                                    '.',
+                                    substr(
+                                        ltrim($valueMainPath, $this->targetDirectoryOperator),
+                                        0,
+                                        -4
+                                    )
+                                ) . ".{$dataKey}"
+                            ] = $result['text'];
+                        }
                         $writeData .= "    '$dataKey' => '" . $result['text'] . "'," . PHP_EOL;
                     }
                     $writeData .= '];';
-                    fwrite($file, $writeData);
-                    fclose($file);
+                    $this->fileWrite("{$realTargetPath}{$valueMainPath}", $writeData);
                 }
             }
         }
+        if ($this->jsLanguageCreator) {
+            if (!file_exists($this->jsSourcePath)) {
+                mkdir($this->jsSourcePath);
+            }
+            foreach ($this->translate as $key => $value) {
+                $this->fileWrite( rtrim($this->jsSourcePath, '/')."/{$key}.lang.js", 'window.language = ' . json_encode($value) . ';');
+            }
+        }
+    }
+
+    /**
+     * File Write
+     * @param string $path
+     * @param string $content
+     * @param string $mode
+     * @return void
+     */
+    private function fileWrite(string $path, string $content, string $mode = 'w') {
+        /*
+         * Dosya yoksa oluşturur
+         */
+        $file = fopen($path, $mode);
+        fwrite($file, $content);
+        fclose($file);
     }
 
     /**
@@ -96,23 +140,18 @@ class Translator
      * Pathları oluşturur
      * @return void
      */
-    public function createPaths() {
+    private function createPaths() {
         $this->sourcePath = "{$this->sourcePrefix}{$this->source}";
         $this->realSourcePath = realpath($this->sourcePath);
     }
 
     /**
      * Dosyaları getirir
-     * @param string $dir
+     * @param null|string $dir
      * @param array $results
      * @return array
      */
-    public function getDirContents(?string $dir = null, array &$results = [])
-    {
-        if ($dir === null) {
-            $this->createPaths();
-            $dir = $this->sourcePath;
-        }
+    public function getDirContents(?string $dir = null, array &$results = []) : array {
         $files = scandir($dir);
         foreach ($files as $key => $value) {
             $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
@@ -145,16 +184,16 @@ class Translator
      * Get Target Directory Operator
      * @return string
      */
-    public function getTargetDirectoryOperator() {
+    public function getTargetDirectoryOperator() : string {
         return $this->targetDirectoryOperator;
     }
 
     /**
      * Set Target Directory Operator
-     * @param $value
+     * @param string $value
      * @return void
      */
-    public function setTargetDirectoryOperator($value) {
+    public function setTargetDirectoryOperator(string $value) {
         $this->targetDirectoryOperator = $value;
     }
 
@@ -162,16 +201,59 @@ class Translator
      * Get Source Prefix
      * @return string
      */
-    public function getSourcePrefix() {
+    public function getSourcePrefix() : string {
         return $this->sourcePrefix;
     }
 
     /**
      * Set Source Prefix
-     * @param $value
+     * @param string $value
      * @return void
      */
-    public function setSourcePrefix($value) {
+    public function setSourcePrefix(string $value) {
         $this->sourcePrefix = $value;
+        $this->createPaths();
+    }
+
+    /**
+     * Get JS Source Path
+     * @return string
+     */
+    public function getJsSourcePath() : string {
+        return $this->jsSourcePath;
+    }
+
+    /**
+     * Set JS Source Path
+     * @param string $value
+     * @return void
+     */
+    public function setJsSourcePath(string $value) {
+        $this->jsSourcePath = $value;
+    }
+
+    /**
+     * Get JS Language Creator
+     * @return string
+     */
+    public function getJsLanguageCreator() : string {
+        return $this->jsLanguageCreator;
+    }
+
+    /**
+     * Set JS Language Creator
+     * @param bool $value
+     * @return void
+     */
+    public function setJsLanguageCreator(bool $value) {
+        /**
+         * Js için diziyi oluşturur
+         */
+        if ($value) {
+            foreach ($this->target as $t) {
+                $this->translate[$t] = [];
+            }
+        }
+        $this->jsLanguageCreator = $value;
     }
 }
